9f0e5df376ddb16ae18ea41e9b39ff626311c22a
ksom10 <someson11@aol.com>
2020-09-28 08:03:38 -0400

    fixed images

--- model/model.js
+++ model/model.js
@@ -7,7 +7,7 @@ var MODEL = (function(){
         var _getHome = function(callback) {
 
             
-            let text = `<h1>This is the Home Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur.</p> <br> <img src="/images/guitar.jpg"/>`;
+            let text = `<h1>This is the Home Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur.</p> <br> <img src="images/guitar.jpg"/>`;
     
             return callback(text);
             
@@ -16,7 +16,7 @@ var MODEL = (function(){
         var _getAbout = function(callback) {
 
             
-            let text = `<h1>This is the About Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur.</p> <br> <img src="/images/recorder.jpg"/>`;
+            let text = `<h1>This is the About Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur.</p> <br> <img src="images/recorder.jpg"/>`;
     
             return callback(text);
             
@@ -25,7 +25,7 @@ var MODEL = (function(){
         var _getProducts = function(callback) {
 
             
-            let text = `<h1>This is the Products Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur. <br> <img src="/images/drums.jpg"/></p>`;
+            let text = `<h1>This is the Products Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur. <br> <img src="images/drums.jpg"/></p>`;
     
             return callback(text);
             
@@ -34,7 +34,7 @@ var MODEL = (function(){
         var _getContact = function(callback) {
 
             
-            let text = `<h1>This is the Contact Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur.</p> <br> <img src="/images/mic.jpg"/>`;
+            let text = `<h1>This is the Contact Page </h1><br><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum obcaecati labore optio ullam debitis minima alias illum odio fuga. Deserunt, repellendus repellat quos distinctio maiores rem assumenda cupiditate officia pariatur.</p> <br> <img src="images/mic.jpg"/>`;
     
             return callback(text);
             
